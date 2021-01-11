de85d1c2b64c49645c1798043b777ee11a5a6607
Gideon kosgei <mr.gkosgei@gmail.com>
2021-01-11 22:11:10 +0300

    batch processing - modal details

--- src/views/Batch/components/Exit/components/add/components/DetailsModal/Details.js
+++ src/views/Batch/components/Exit/components/add/components/DetailsModal/Details.js
@@ -2,18 +2,17 @@ import React from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
-import PerfectScrollbar from 'react-perfect-scrollbar';
-import { Modal,Card,CardContent, CardActions,Typography,Button, colors} from '@material-ui/core';
+import {Modal,Card, CardContent, CardHeader, Grid,Divider, TextField,colors,Button,CardActions } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
     position: 'absolute',
-    top: '20%',
+    top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     outline: 'none',
     boxShadow: theme.shadows[20],
-    width: '80%',
+    width: '60%',
     maxHeight: '80%',
     overflowY: 'auto',
     maxWidth: '100%'
@@ -47,7 +46,47 @@ const useStyles = makeStyles(theme => ({
       records.push(data[i]);
     }
   }
-  let values = records[0];
+  
+  let values = records[0];  
+  let created_by = "";
+  let created_date = "";
+  let created_time = "";
+  let tag_id = "";
+  let animal_id = "";
+  let animal_name = "";
+  let disposal_amount= "";
+  let disposal_reason = "";
+  let exit_date = "";
+  let new_breeder_name = "";
+  let new_breeder_phone_number = "";
+  let new_country = "";
+  let new_district = "";
+  let new_farmer_name = "";
+  let new_farmer_phone_number = "";
+  let new_region = "";
+  let new_village = "";
+  let new_ward = "";
+ 
+  if (typeof values != 'undefined'){
+     animal_id = values.animal_id;
+     created_by = values.created_by;
+     created_date = values.created_date;
+     created_time = values.created_time;
+     tag_id = values.tag_id;
+     animal_name = values.animal_name; 
+     disposal_amount= values.disposal_amount; 
+      disposal_reason= values.disposal_reason; 
+      exit_date= values.exit_date; 
+      new_breeder_name= values.new_breeder_name; 
+      new_breeder_phone_number= values.new_breeder_phone_number; 
+      new_country= values.new_country; 
+      new_district= values.new_district; 
+      new_farmer_name= values.new_farmer_name; 
+      new_farmer_phone_number= values.new_farmer_phone_number; 
+      new_region= values.new_region; 
+      new_village= values.new_village; 
+      new_ward= values.new_ward;      
+  }
 
   return (
     <Modal
@@ -57,21 +96,451 @@ const useStyles = makeStyles(theme => ({
       <Card
         {...rest}
         className={clsx(classes.root, className)}
-      >
-        <form>
-          <CardContent>
-            <Typography
-              align="center"
-              gutterBottom
-              variant="h5"
-            >
-             Validation Error Log - <span className={classes.details}> #{record_id}</span>
-            </Typography>
-            <PerfectScrollbar>
+      >      
+          <CardContent> 
+          <CardHeader title= "EXIT & DISPOSAL RECORD"/>
+           <Divider />
           <div className={classes.inner}>
+            <br/>
+          <Grid
+                container
+                spacing={4}
+              > 
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                    <TextField
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{
+                        readOnly: true,
+                        disabled: true ,                        
+                             
+                      }}                      
+                      margin = 'dense'
+                      label="Animal ID"                      
+                      name="animal_id"  
+                      value = {animal_id}  
+                      variant="outlined" 
+                     
+                    />
+                  </Grid>
+                  <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                    <TextField
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{
+                        readOnly: true,
+                        disabled: true ,                        
+                             
+                      }}                      
+                      margin = 'dense'
+                      label="Tag ID"
+                      value = {tag_id}  
+                      variant="outlined" 
+                     
+                    />
+                  </Grid>
+                  <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                    <TextField
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{
+                        readOnly: true,
+                        disabled: true ,                        
+                             
+                      }}                      
+                      margin = 'dense'
+                      label="Animal Name"
+                      value = {animal_name}  
+                      variant="outlined" 
+                     
+                    />
+                  </Grid>
+                  
+                  <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                    <TextField
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{
+                        readOnly: true,
+                        disabled: true ,                        
+                             
+                      }}                      
+                      margin = 'dense'
+                      label="Exit Date" 
+                      value = {exit_date}  
+                      variant="outlined" 
+                     
+                    />
+                  </Grid>
+                
+                <Grid
+                  item
+                  md={6}
+                  xs={12}
+                >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true                     
+                    }}                    
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true   
+                    }} 
+                    //required
+                    margin = 'dense'
+                    label="Disposal Reason" 
+                    variant="outlined"  
+                    value = {disposal_reason}    
+                    multiline      
+                    rowsMax = {3}
+                    rows={1}                                           
+                  />
+                </Grid>
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                      
+                    }}
+                    //required
+                    margin = 'dense'
+                    label="Disposal Amount"
+                    variant="outlined"                                      
+                    value = {disposal_amount}   
+                />
+              </Grid>                
+                <Grid
+                      item
+                      md={3}
+                      xs={12}
+                    >
+                    <TextField
+                       fullWidth
+                       InputLabelProps={{
+                         shrink: true,
+                       }}
+   
+                       inputProps={{
+                         readOnly: true,
+                         disabled: true                                    
+                       }}                      
+                       margin = 'dense'
+                       label="New Breeder Name"                      
+                       variant="outlined"                                       
+                       value = {new_breeder_name}     
+                  />
+                </Grid>
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="New Breeder Phone Number"                                   
+                    value = {new_breeder_phone_number}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+                
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="New Farmer Name"                                   
+                    value = {new_farmer_name}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="New Farmer Phone Number"                                   
+                    value = {new_farmer_phone_number}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="New Country"                                   
+                    value = {new_country}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="New District"                                   
+                    value = {new_district}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="New Region"                                   
+                    value = {new_region}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="New Village"                                   
+                    value = {new_village}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="New Ward"                                   
+                    value = {new_ward}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+                
+                
+                
+                
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="Created By"
+                    name="created_by"                   
+                    value = {created_by}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+                    margin = 'dense'
+                    label = 'Created Date'                                                      
+                    value = {created_date}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+               
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="Time Created"                                     
+                    value = {created_time}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+               
+               
+              </Grid>
+         
+         
+
             
-         </div>
-        </PerfectScrollbar>
+         
+
+
+          
+         </div>       
           </CardContent>
           <CardActions className={classes.actions}>
             <Button
@@ -81,8 +550,7 @@ const useStyles = makeStyles(theme => ({
             >
               Close
             </Button>           
-          </CardActions>
-        </form>
+          </CardActions>        
       </Card>
     </Modal>
   );
