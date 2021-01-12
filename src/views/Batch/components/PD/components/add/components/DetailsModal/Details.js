522d39620297124faba399ad69470746393b487f
Gideon kosgei <mr.gkosgei@gmail.com>
2021-01-12 14:35:40 +0300

    batch operations - details modal

--- src/views/Batch/components/PD/components/add/components/DetailsModal/Details.js
+++ src/views/Batch/components/PD/components/add/components/DetailsModal/Details.js
@@ -2,18 +2,17 @@ import React from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
-import PerfectScrollbar from 'react-perfect-scrollbar';
-import { Modal,Card,CardContent, CardActions,Typography,Button, colors} from '@material-ui/core';
+import {Modal,Card, CardContent, CardHeader, Grid,Divider, TextField,colors,Button,CardActions } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
     position: 'absolute',
-    top: '20%',
+    top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     outline: 'none',
     boxShadow: theme.shadows[20],
-    width: '80%',
+    width: '60%',
     maxHeight: '80%',
     overflowY: 'auto',
     maxWidth: '100%'
@@ -47,7 +46,35 @@ const useStyles = makeStyles(theme => ({
       records.push(data[i]);
     }
   }
-  let values = records[0];
+  
+  let values = records[0];  
+  let created_by = "";
+  let created_date = "";
+  let created_time = "";
+  let tag_id = "";
+  let animal_id = "";
+  let animal_name = "";  
+  let exam_date = "";  
+  let pd_cost = "";  
+  let pd_method = "";  
+  let pd_stage = "";  
+  let body_condition = ""; 
+  let pd_result = "";    
+ 
+  if (typeof values != 'undefined'){    
+     animal_id = values.animal_id;
+     created_by = values.created_by;
+     created_date = values.created_date;
+     created_time = values.created_time;
+     tag_id = values.tag_id;
+     animal_name = values.animal_name; 
+     exam_date = values.exam_date;
+     pd_cost = values.pd_cost;
+     pd_method = values.pd_method;
+     pd_stage = values.pd_stage;
+     body_condition = values.body_condition;  
+     pd_result = values.pd_result;    
+  }
 
   return (
     <Modal
@@ -57,21 +84,305 @@ const useStyles = makeStyles(theme => ({
       <Card
         {...rest}
         className={clsx(classes.root, className)}
-      >
-        <form>
-          <CardContent>
-            <Typography
-              align="center"
-              gutterBottom
-              variant="h5"
-            >
-             Validation Error Log - <span className={classes.details}> #{record_id}</span>
-            </Typography>
-            <PerfectScrollbar>
+      >      
+          <CardContent> 
+          <CardHeader title= "EXIT & DISPOSAL RECORD"/>
+           <Divider />
           <div className={classes.inner}>
+            <br/>
+          <Grid
+                container
+                spacing={4}
+              > 
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                    <TextField
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{
+                        readOnly: true,
+                        disabled: true ,                        
+                             
+                      }}                      
+                      margin = 'dense'
+                      label="Animal ID"                      
+                      name="animal_id"  
+                      value = {animal_id}  
+                      variant="outlined" 
+                     
+                    />
+                  </Grid>
+                  <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                    <TextField
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{
+                        readOnly: true,
+                        disabled: true ,                        
+                             
+                      }}                      
+                      margin = 'dense'
+                      label="Tag ID"
+                      value = {tag_id}  
+                      variant="outlined" 
+                     
+                    />
+                  </Grid>
+                  <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                    <TextField
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{
+                        readOnly: true,
+                        disabled: true ,                        
+                             
+                      }}                      
+                      margin = 'dense'
+                      label="Animal Name"
+                      value = {animal_name}  
+                      variant="outlined" 
+                     
+                    />
+                  </Grid>
+                  
+                  <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                    <TextField
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{
+                        readOnly: true,
+                        disabled: true ,                        
+                             
+                      }}                      
+                      margin = 'dense'
+                      label="Exam Date" 
+                      value = {exam_date}  
+                      variant="outlined" 
+                     
+                    />
+                  </Grid>
+                
+                <Grid
+                  item
+                  md={3}
+                  xs={12}
+                >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true                     
+                    }}                    
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true   
+                    }} 
+                    //required
+                    margin = 'dense'
+                    label="PD Method" 
+                    variant="outlined"  
+                    value = {pd_method}                        
+                  />
+                </Grid>
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                      
+                    }}
+                    //required
+                    margin = 'dense'
+                    label="PD Result"
+                    variant="outlined"                                      
+                    value = {pd_result}   
+                />
+              </Grid>                
+                <Grid
+                      item
+                      md={3}
+                      xs={12}
+                    >
+                    <TextField
+                       fullWidth
+                       InputLabelProps={{
+                         shrink: true,
+                       }}
+   
+                       inputProps={{
+                         readOnly: true,
+                         disabled: true                                    
+                       }}                      
+                       margin = 'dense'
+                       label="PD Stage"                      
+                       variant="outlined"                                       
+                       value = {pd_stage}     
+                  />
+                </Grid>
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="PD Cost"                                   
+                    value = {pd_cost}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+                
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="Body Condition"                                   
+                    value = {body_condition}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+               
+              
+                
+                
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="Created By"
+                    name="created_by"                   
+                    value = {created_by}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+                    margin = 'dense'
+                    label = 'Created Date'                                                      
+                    value = {created_date}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+               
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="Time Created"                                     
+                    value = {created_time}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+               
+               
+              </Grid>
+         
+         
+
             
-         </div>
-        </PerfectScrollbar>
+         
+
+
+          
+         </div>       
           </CardContent>
           <CardActions className={classes.actions}>
             <Button
@@ -81,8 +392,7 @@ const useStyles = makeStyles(theme => ({
             >
               Close
             </Button>           
-          </CardActions>
-        </form>
+          </CardActions>        
       </Card>
     </Modal>
   );
