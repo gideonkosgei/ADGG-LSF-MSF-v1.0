d3890f73b886cee8694a238dfbfe611db73cbae8
Gideon kosgei <mr.gkosgei@gmail.com>
2021-01-12 17:14:16 +0300

    sync batch operations - details modal

--- src/views/Batch/components/Sync/components/add/components/DetailsModal/Details.js
+++ src/views/Batch/components/Sync/components/add/components/DetailsModal/Details.js
@@ -2,18 +2,17 @@ import React from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
-import PerfectScrollbar from 'react-perfect-scrollbar';
-import { Modal,Card,CardContent, CardActions,Typography,Button, colors} from '@material-ui/core';
+import {Modal,Card, CardContent, CardHeader, Grid,Divider, TextField,colors,Button,CardActions } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
     position: 'absolute',
-    top: '20%',
+    top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     outline: 'none',
     boxShadow: theme.shadows[20],
-    width: '80%',
+    width: '60%',
     maxHeight: '80%',
     overflowY: 'auto',
     maxWidth: '100%'
@@ -47,7 +46,39 @@ const useStyles = makeStyles(theme => ({
       records.push(data[i]);
     }
   }
-  let values = records[0];
+  
+  let values = records[0];  
+  let created_by = "";
+  let created_date = "";
+  let created_time = "";
+  let tag_id = "";
+  let animal_id = "";
+  let animal_name = ""; 
+  let cost= "";
+  let admin_name= "";
+  let admin_phone= "";
+  let hormone_source= "";
+  let hormone_type= "";
+  let sync_date= "";
+  let sync_number= "";
+ 
+  if (typeof values != 'undefined'){    
+   
+     animal_id = values.animal_id;
+     created_by = values.created_by;
+     created_date = values.created_date;
+     created_time = values.created_time;
+     tag_id = values.tag_id;
+     animal_name = values.animal_name; 
+
+    cost= values.cost; 
+    admin_name= values.admin_name; 
+    admin_phone= values.admin_phone; 
+    hormone_source= values.hormone_source; 
+    hormone_type= values.hormone_type; 
+    sync_date= values.sync_date; 
+    sync_number= values.sync_number;   
+  }
 
   return (
     <Modal
@@ -57,21 +88,329 @@ const useStyles = makeStyles(theme => ({
       <Card
         {...rest}
         className={clsx(classes.root, className)}
-      >
-        <form>
-          <CardContent>
-            <Typography
-              align="center"
-              gutterBottom
-              variant="h5"
-            >
-             Validation Error Log - <span className={classes.details}> #{record_id}</span>
-            </Typography>
-            <PerfectScrollbar>
+      >      
+          <CardContent> 
+          <CardHeader title= "SYNCHRONIZATION RECORD"/>
+           <Divider />
           <div className={classes.inner}>
+            <br/>
+          <Grid
+                container
+                spacing={4}
+              > 
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                    <TextField
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{
+                        readOnly: true,
+                        disabled: true ,                        
+                             
+                      }}                      
+                      margin = 'dense'
+                      label="Animal ID"                      
+                      name="animal_id"  
+                      value = {animal_id}  
+                      variant="outlined" 
+                     
+                    />
+                  </Grid>
+                  <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                    <TextField
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{
+                        readOnly: true,
+                        disabled: true ,                        
+                             
+                      }}                      
+                      margin = 'dense'
+                      label="Tag ID"
+                      value = {tag_id}  
+                      variant="outlined" 
+                     
+                    />
+                  </Grid>
+                  <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                    <TextField
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{
+                        readOnly: true,
+                        disabled: true ,                        
+                             
+                      }}                      
+                      margin = 'dense'
+                      label="Animal Name"
+                      value = {animal_name}  
+                      variant="outlined" 
+                     
+                    />
+                  </Grid>
+                  
+                  <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                    <TextField
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      inputProps={{
+                        readOnly: true,
+                        disabled: true ,                        
+                             
+                      }}                      
+                      margin = 'dense'
+                      label="Sync Date" 
+                      value = {sync_date}  
+                      variant="outlined" 
+                     
+                    />
+                  </Grid>
+                
+                <Grid
+                  item
+                  md={3}
+                  xs={12}
+                >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true                     
+                    }}                    
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true   
+                    }} 
+                    //required
+                    margin = 'dense'
+                    label="Hormone Source" 
+                    variant="outlined"  
+                    value = {hormone_source}                        
+                  />
+                </Grid>
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                      
+                    }}
+                    //required
+                    margin = 'dense'
+                    label="Hormone Type"
+                    variant="outlined"                                      
+                    value = {hormone_type}   
+                />
+              </Grid>                
+                <Grid
+                      item
+                      md={3}
+                      xs={12}
+                    >
+                    <TextField
+                       fullWidth
+                       InputLabelProps={{
+                         shrink: true,
+                       }}
+   
+                       inputProps={{
+                         readOnly: true,
+                         disabled: true                                    
+                       }}                      
+                       margin = 'dense'
+                       label="Sync Number"                      
+                       variant="outlined"                                       
+                       value = {sync_number}     
+                  />
+                </Grid>
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="Sync Cost"                                   
+                    value = {cost}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+
+
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="Admin Name"                                   
+                    value = {admin_name}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="Admin Phone Number"                                   
+                    value = {admin_phone}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+              
+                
+                
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="Created By"
+                    name="created_by"                   
+                    value = {created_by}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+                    margin = 'dense'
+                    label = 'Created Date'                                                      
+                    value = {created_date}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+               
+                <Grid
+                    item
+                    md={3}
+                    xs={12}
+                  >
+                  <TextField
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+
+                    inputProps={{
+                      readOnly: true,
+                      disabled: true                
+                    }}
+
+                    margin = 'dense'
+                    label="Time Created"                                     
+                    value = {created_time}                                        
+                    variant="outlined"
+                  />
+                   
+                </Grid>
+               
+               
+              </Grid>
+         
+         
+
             
-         </div>
-        </PerfectScrollbar>
+         
+
+
+          
+         </div>       
           </CardContent>
           <CardActions className={classes.actions}>
             <Button
@@ -81,8 +420,7 @@ const useStyles = makeStyles(theme => ({
             >
               Close
             </Button>           
-          </CardActions>
-        </form>
+          </CardActions>        
       </Card>
     </Modal>
   );
