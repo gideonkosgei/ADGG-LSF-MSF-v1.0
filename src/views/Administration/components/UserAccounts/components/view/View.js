2b69415cd799b92b8d3ea2a9a459db04e2fd449e
Gideon kosgei <mr.gkosgei@gmail.com>
2021-04-10 12:15:55 +0300

    user accounts-  added loader & modified page header

--- src/views/Administration/components/UserAccounts/components/view/View.js
+++ src/views/Administration/components/UserAccounts/components/view/View.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState,useEffect,useContext} from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
-import { Card,CardContent, colors } from '@material-ui/core';
+import {Card, CardContent,LinearProgress,Divider, Grid,colors,Link ,Typography} from '@material-ui/core';
+import {genericFunctionFourParameters}   from '../../../../../../utils/API';
+import {endpoint_user_list} from '../../../../../../configs/endpoints';
+import MUIDataTable from "mui-datatables";
+import {MuiThemeProvider } from '@material-ui/core/styles';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import { Link as RouterLink } from 'react-router-dom';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import { Page } from 'components';
-import {default as DetailsView} from './DetailsView';
-
-
+import CustomToolbar from "../CustomToolbar";
+import authContext from '../../../../../../contexts/AuthContext';
 
 const useStyles = makeStyles(theme => ({
-  root: {},
+  root: {
+    width: theme.breakpoints.values.lg,
+    maxWidth: '100%',
+    margin: '0 auto',
+    padding: theme.spacing(3)
+  },
   inner: {
     width: theme.breakpoints.values.lg,
     maxWidth: '100%',
@@ -24,18 +35,116 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const Edit = props => { 
-  const classes = useStyles(); 
+  const classes = useStyles();  
+  const [values, setValues] = useState([]);
+  const [ {organization_id} ] = useContext(authContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {     
+    let mounted = true;
+
+      (async  (endpoint,desc,option,org) => {     
+        await  genericFunctionFourParameters(endpoint,desc,option,org)
+        .then(response => {                        
+          if (mounted) {            
+            setValues(response.payload[0]);   
+            setIsLoading(false);                
+          }
+        });
+      })(endpoint_user_list,'Org user list',1,organization_id);
+
+    return () => {
+      mounted = false;           
+    };
+  }, [organization_id]); 
+
+  if (!values) {
+    return null;
+  } 
+ 
+    const columns = [
+    { name: "id",label: "ID",options: {filter: false,sort: false,display:false}},   
+    { name: "name",label: "Name",options: {filter: false,sort: true,display:true}},
+    { name: "username",label: "User Name",options: {filter: false,sort: true,display:true}},
+    { name: "email",label: "Email Address",options: {filter: false,sort: false,display:true}},
+    { name: "phone",label: "Phone",options: {filter:false,sort: true,display:true}},        
+    { name: "role",label: "Role",options: {filter: true,sort: false,display:true}}, 
+    { name: "status",label: "Status",options: {filter: true,sort: false,display:true}},
+    { name: "created_at",label: "Date Created",options: {filter: false,sort: false,display:true}},  
+    { name: "",
+      options: {
+      filter: false,
+      sort: false,  
+      empty:true,    
+      customBodyRender: (value, tableMeta, updateValue) => {        
+        return (
+          <Link
+              component={RouterLink}             
+              to = {`/administration/org/user-accounts/edit/${tableMeta.rowData[0]}`}               
+          >
+            <OpenInNewIcon/>
+          </Link>          
+        );
+      }
+    }
+  }    
+  ];   
+  const options = {       
+    filter: true,
+    rowsPerPage: 5,       
+    rowsPerPageOptions :[5,10,20,50,100],
+    selectableRows: 'none',      
+    filterType: 'checkbox',
+    responsive: 'stacked',                
+    rowHover: true,       
+    setTableProps: () => {
+     return {
+       padding: "none" ,         
+       size: "small",
+     };
+   },
+   customToolbar: () => {
+    return (
+      <CustomToolbar />
+    );
+  }    
+  };
   return (
+    
     <Page
       className={classes.root}
-      title="Organization"
+      title="user accounts"
     >
-    <Card>
-        <CardContent>
-            <DetailsView/>
-        </CardContent>
-    </Card>
-     
+       <Typography
+        component="h1"
+        gutterBottom
+        variant="h3"
+      >
+       USER ACCOUNTS
+      </Typography>
+      { isLoading  &&
+        <LinearProgress/>
+      }  
+      <Divider />  
+      <br/>   
+        <Grid container spacing={1} justify="center">  
+            <Grid item xs={12}>
+              <Card > 
+                <CardContent>                 
+                  <PerfectScrollbar>                  
+                      <MuiThemeProvider>                
+                        <MUIDataTable
+                          title = ""
+                          data={values}
+                          columns={columns}
+                          options={options}
+                        />
+                      </MuiThemeProvider>                    
+                  </PerfectScrollbar> 
+                </CardContent>
+              </Card> 
+          </Grid>
+        </Grid>        
    </Page>
   );
 };
