8c5e3426a240f97adf198585285ada60dd3e9453
GKosgei <mr.gkosgei@gmail.com>
2022-05-31 21:33:05 +0300

    configs - added live url

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,4 +1,5 @@
 export const api = `http://localhost:8080/api/v1.0`;
+//export const api = "https://18.118.109.57/api/v1.0";
 const  token = "Basic amFjazpzZWNyZXQ="; 
 export const headers = {
       'Accept': 'application/json',
